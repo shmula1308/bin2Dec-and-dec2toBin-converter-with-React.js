f08cea71763bfc5f0fce3cfae3e0b95760f38996
Shpend Mula <shmula@hotmail.com>
2021-05-28 12:47:03 +0200

    updated css modules

--- src/components/Form/Select/Select.js
+++ src/components/Form/Select/Select.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import classes from './Select.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown} from '@fortawesome/free-solid-svg-icons'
+
 
 const Select = () => {
+    
     return (
-        <div>
-            <div className="form__select-container">
-                <i className="fas fa-sort-down form__select-icon"></i>
-                <label htmlFor="binToDec" className="form__select-label">From</label>
-                <select id="binToDec" className="form__select">
+        <div className={classes['form__select-component']}>
+            <div className={classes['form__select-container']}>
+            <FontAwesomeIcon icon={faSortDown} className={classes["form__select-icon"]}/>
+                <label htmlFor="binToDec" className={classes["form__select-label"]}>From</label>
+                <select id="binToDec" className={classes.form__select}>
                     <option value="binary">Binary</option>
                     <option value="decimal">Decimal</option>
                 </select>
             </div>
-            <div className="form__select-container">
-                <i className="fas fa-sort-down"></i>
-                <label htmlFor="decToBin" className="form__select-label">To</label>
-                <select id="decToBin" className="form__select">
+            <div className={classes['form__select-container']}>
+                <FontAwesomeIcon icon={faSortDown} className={classes["form__select-icon"]}/>
+                <label htmlFor="decToBin" className={classes["form__select-label"]}>To</label>
+                <select id="decToBin" className={classes.form__select}>
                     <option value="decimal">Decimal</option>
                     <option value="binary">Binary</option>
                 </select>
